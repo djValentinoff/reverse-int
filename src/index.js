51a636bcd18489c0287f23bddc41eaa534ee21a9
Aliaksandr <djvalentinoff@gmail.com>
2022-07-04 19:46:49 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let nReverse = 0;
+
+  if (n > 0) {
+    while (n != 0) {
+      nReverse *= 10;
+      nReverse += n % 10;
+      n -= n % 10;
+      n /= 10;
+    }
+  } else if (n < 0) {
+    n = -n
+    while (n != 0) {
+      nReverse *= 10;
+      nReverse += n % 10;
+      n -= n % 10;
+      n /= 10;
+    }
+  }
+
+  return nReverse;
 }
